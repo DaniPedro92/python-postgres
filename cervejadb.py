848ef20c23f007bb9c0ce2871d53cf97fc71a96c
Filipe Daniel Pedro <filipedpedro@gmail.com>
2024-05-17 16:56:10 +0100

    alteracao da disposicao dos dados

--- cervejadb.py
+++ cervejadb.py
@@ -1,9 +1,27 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
+from tabulate import tabulate
 
-# Carrega as variáveis de ambiente do arquivo test.env
 load_dotenv("test.env")
 
-print("PG_HOST:", os.getenv('PG_HOST'))
-print("PG_PORT:", os.getenv('PG_PORT'))
+try:
+    conn = psycopg2.connect(
+        dbname=os.getenv('PG_DATABASE'),
+        user=os.getenv('PG_USER'),
+        password=os.getenv('PG_PASSWORD'),
+        host=os.getenv('PG_HOST'),
+        port=os.getenv('PG_PORT')
+    )
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, nome, cervejaria, estilo, teor_alcoolico, volume_ml, preco, quantidade_estoque, data_validade FROM cervejas;')
+    results = cursor.fetchall()
+    conn.close()
+
+    table_data = [list(row) for row in results]
+    column_names = [desc[0] for desc in cursor.description]
+    print(tabulate(table_data, headers=column_names, tablefmt="grid"))
+
+except psycopg2.Error as e:
+    print(f"Erro ao conectar ao PostgreSQL: {e}")
