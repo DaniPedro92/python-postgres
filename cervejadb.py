bc372a7ae2357f7287665d37612d1088bb65545b
Filipe Daniel Pedro <filipedpedro@gmail.com>
2024-05-17 16:05:43 +0100

    test10

--- cervejadb.py
+++ cervejadb.py
@@ -1,29 +1,37 @@
 import psycopg2
 import os
 
-password = os.getenv('PG_PASSWORD')
+# Obtenha as informações de conexão do ambiente
+host = os.getenv('PG_HOST')
+port = os.getenv('PG_PORT')
+database = os.getenv('PG_DATABASE')
 user = os.getenv('PG_USER')
-host = os.getenv('PG_HOST_DB')
+password = os.getenv('PG_PASSWORD')
 
 try:
+    # Conecte-se ao banco de dados usando as informações do ambiente
     conn = psycopg2.connect(
-        dbname="db-fdp",
-        user=user,
-        password=password,
         host=host,
-        port="5432"
+        port=port,
+        database=database,
+        user=user,
+        password=password
     )
 
     cursor = conn.cursor()
-    cursor.execute('SELECT id, nome, cervejaria, estilo, teor_alcoolico, volume_ml, preco, quantidade_estoque, data_validade FROM cervejas;')
+    query = "SELECT * FROM public.contacts"
+    cursor.execute(query)
+
     results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
     conn.close()
 
-    for x in results:
-        print(x)
-except psycopg2.OperationalError as e:
-    print(f"OperationalError: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+except psycopg2.Error as e:
+    print(f"Erro ao conectar ao PostgreSQL: {e}")
+
+
 
 
