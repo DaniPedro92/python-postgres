5777913aa3dc7ef18b2d9eb2fba891f31ca6b5ce
Filipe Daniel Pedro <filipedpedro@gmail.com>
2024-05-17 15:40:50 +0100

    test6

--- cervejadb.py
+++ cervejadb.py
@@ -4,24 +4,26 @@ import os
 password = os.getenv('PG_PASSWORD')
 user = os.getenv('PG_USER')
 host = os.getenv('PG_HOST_DB')
-#dbname = os.getenv('PG_DB')
 
+try:
+    conn = psycopg2.connect(
+        dbname="db-fdp",
+        user=user,
+        password=password,
+        host=host,
+        port="5432"
+    )
 
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, nome, cervejaria, estilo, teor_alcoolico, volume_ml, preco, quantidade_estoque, data_validade FROM cervejas;')
+    results = cursor.fetchall()
+    conn.close()
 
-conn = psycopg2.connect(
-    dbname="db-fdp",
-    user=user,
-    password=password,
-    host=host,
-    port="5432"
-)
-
-cursor = conn.cursor()
-cursor.execute('SELECT id, nome, cervejaria, estilo, teor_alcoolico, volume_ml, preco, quantidade_estoque, data_validade FROM cervejas;')
-results = cursor.fetchall()
-conn.close()
-
-for x in results:
-    print(x)
+    for x in results:
+        print(x)
+except psycopg2.OperationalError as e:
+    print(f"OperationalError: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 
